a544eca720468b8ba32e621a565a15144792276a
pmbrull <p.brull@stuart.com>
2022-01-26 19:46:12 +0100

    Add stars evolution

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def get_all(path: Path):
     return data
 
 
-def cook_stars():
+def cook_stars_data():
     today = datetime.today()
     delta = today - START_DATE
 
@@ -72,11 +72,25 @@ def cook_stars():
     return df
 
 
-def stats():
-    st.title("OpenMetadata - Community Stats")
+def cook_stars_app():
+    df = cook_stars_data()
+
+    current = int(df.iloc[-1].get("stars"))
+    last_week = int(df.iloc[-2].get("stars"))
+    last_month = int(df.iloc[-4].get("stars"))
 
     st.subheader("Stars evolution")
-    st.line_chart(cook_stars())
+    st.line_chart(df)
+
+    star_current, star_weekly, star_monthly = st.columns(3)
+    star_current.metric("Current stars", current)
+    star_weekly.metric("Last week inc.", last_week, current - last_week)
+    star_monthly.metric("Last month inc.", last_month, current - last_month)
+
+
+def stats():
+    st.title("OpenMetadata - Community Stats")
+    cook_stars_app()
 
 
 if __name__ == "__main__":
