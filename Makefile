9f64240eb195eee42ec5a7136cb4d7bceb997b45
pmbrull <peremiquelbrull@gmail.com>
2022-02-04 19:19:36 +0100

    Use openstats

--- Makefile
+++
@@ -1,6 +0,0 @@
-run:
-	python -m streamlit run app.py
-
-py_format:  ## Run black and isort to format the Python codebase
-	isort . --profile black --multi-line 3
-	black .
--- app.py
+++ app.py
@@ -1,27 +1,4 @@
-"""
-Main file to run the streamlit app
-"""
-import streamlit as st
-
-from stats.components import (
-    clear_cache_button,
-    contributors_component,
-    good_first_issues_component,
-    stars_component, traffic_component, sidebar,
-)
-
-
-def stats():
-    st.title("OpenMetadata - Community Stats")
-    sidebar()
-    stars_component()
-    st.markdown("---")
-    good_first_issues_component()
-    st.markdown("---")
-    contributors_component()
-    st.markdown("---")
-    traffic_component()
-
+from openstats.app import run
 
 if __name__ == "__main__":
-    stats()
+    run()
